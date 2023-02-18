743a84cbc3c6483a0db6d8e5d77efaff1a7ee97e
kulagin <kulagin@WIN-SFUQ8BAFB06>
2023-02-18 15:15:35 +0400

    wordbooks fixed?

--- LessonsBotDbProvider/ModelsDb/WordBook.cs
+++ LessonsBotDbProvider/ModelsDb/WordBook.cs
@@ -5,8 +5,59 @@ namespace LessonsBotDbProvider.ModelsDb
     public class WordBook
     {
         [Key]
-        public long IdWord { get; set; }
-        public string Question { get; set; }
-        public string Answer { get; set; }
+        public Guid IdWord { get; set; }
+
+        protected List<string> _questions;
+
+        public string Questions
+        {
+            set
+            {
+                _questions = value.Split(';').ToList();
+            }
+            
+            get
+            {
+                string data = "";
+
+                foreach (var item in _questions)
+                {
+                    data += $"{item};";
+                }
+
+                return data;
+            }
+        }
+
+        protected List<string> _answers;
+
+        public string Answers
+        {
+
+            set
+            {
+                _answers = value.Split(';').ToList();
+            }
+
+            get
+            {
+                string data = "";
+
+                foreach (var item in _answers)
+                {
+                    data += $"{item};";
+                }
+
+                return data;
+            }
+        }
+
+        public WordBook()
+        {
+            _questions = new List<string>();
+            _answers = new List<string>();
+        }
+
+
     }
 }
